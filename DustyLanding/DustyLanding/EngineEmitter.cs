e4be11649b1cb8e95e8796d18bc2f623d59fb520
Ingo Bürk <ingo.buerk@tngtech.com>
2014-01-25 17:24:15 +0100

    move method for creating particle emitter into EngineEmitter

--- DustyLanding/DustyLanding/EngineEmitter.cs
+++ DustyLanding/DustyLanding/EngineEmitter.cs
@@ -9,10 +9,11 @@ public class EngineEmitter {
 	private readonly ParticleEmitter emitter;
 	private readonly Transform thruster;
 
-	public EngineEmitter( Part part, ParticleEmitter emitter, Transform thruster ) {
+	public EngineEmitter( Part part, Transform thruster ) {
 		this.part = part;
-		this.emitter = emitter;
 		this.thruster = thruster;
+
+		this.emitter = CreateParticleEmitter();
 	}
 
 	public void Process() {
@@ -48,5 +49,25 @@ public class EngineEmitter {
 		if( emitter.particleCount == 0 ) {
 			emitter.gameObject.SetActive( false );
 		}
+	}
+
+	private ParticleEmitter CreateParticleEmitter() {
+		GameObject emitterGameObject = (GameObject) UnityEngine.Object.Instantiate( UnityEngine.Resources.Load( "Effects/fx_smokeTrail_light" ) );
+		emitterGameObject.transform.parent = part.transform;
+		emitterGameObject.transform.localRotation = Quaternion.identity;
+		emitterGameObject.SetActive( false );
+
+		ParticleEmitter emitter = emitterGameObject.GetComponent<ParticleEmitter>();
+		emitter.maxEmission = 10;
+		emitter.maxEnergy = 5;
+		emitter.maxSize = 7;
+		emitter.minEmission = 3;
+		emitter.minEnergy = 1;
+		emitter.minSize = 1;
+
+		// TODO this can be updated in each update cycle to account for tilt
+		emitter.localVelocity = new Vector3( 0, 0, 0 );
+
+		return emitter;
 	}
 }
--- DustyLanding/DustyLanding/EngineEmitters.cs
+++ DustyLanding/DustyLanding/EngineEmitters.cs
@@ -10,7 +10,7 @@ public class EngineEmitters {
 	public EngineEmitters( Part part, DualModuleEngines engine ) {
 		this.engine = engine;
 		foreach( Transform thrust in engine.thrustTransforms ) {
-			emitters.Add( new EngineEmitter( part, CreateParticleEmitter( engine.module.part ), thrust ) );
+			emitters.Add( new EngineEmitter( part, thrust ) );
 		}
 	}
 
@@ -25,25 +25,5 @@ public class EngineEmitters {
 
 	private bool IsEngineActive() {
 		return engine.isEnabled && engine.isIgnited && !engine.isFlameout && engine.HasThrust();
-	}
-
-	private ParticleEmitter CreateParticleEmitter( Part part ) {
-		GameObject emitterGameObject = (GameObject) UnityEngine.Object.Instantiate( UnityEngine.Resources.Load( "Effects/fx_smokeTrail_light" ) );
-		emitterGameObject.transform.parent = part.transform;
-		emitterGameObject.transform.localRotation = Quaternion.identity;
-		emitterGameObject.SetActive( false );
-
-		ParticleEmitter emitter = emitterGameObject.GetComponent<ParticleEmitter>();
-		emitter.maxEmission = 10;
-		emitter.maxEnergy = 5;
-		emitter.maxSize = 7;
-		emitter.minEmission = 3;
-		emitter.minEnergy = 1;
-		emitter.minSize = 1;
-
-		// TODO this can be updated in each update cycle to account for tilt
-		emitter.localVelocity = new Vector3( 0, 0, 0 );
-
-		return emitter;
 	}
 }
